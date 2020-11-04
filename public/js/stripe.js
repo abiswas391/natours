6ddee399d2bb1b9d29b9078811c58b84a2db43cf
Abhijit Biswas <abiswas391@gmail.com>
2020-11-04 22:17:32 +0530

    Modified api url

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -6,9 +6,7 @@ const stripe = Stripe(
 const bookTour = async tourId => {
   try {
     // 1) Get the session from the server/API
-    const session = await axios(
-      `http://127.0.0.1:8000/api/v1/bookings/checkout-session/${tourId}`
-    );
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
     // console.log(session);
 
     // 2) Create the checkout form + charge the credit card
